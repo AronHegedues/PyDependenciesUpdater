551bb4d79fc90dccb53eb7ca2ed58b0b2fdbfdcb
Simplicitus <50057034+Simplicitus@users.noreply.github.com>
2019-12-06 23:02:01 +0100

    Update PyUpdater.py

--- PyUpdater.py
+++ PyUpdater.py
@@ -6,20 +6,20 @@ from bs4 import BeautifulSoup
 
 try:
 
-    # Alle Dependencies bekommen und upgraden
+    # get all dependencies and upgrade them
     packages = [dist.project_name for dist in pkg_resources.working_set]
     call("python.exe -m pip install --upgrade " + ' '.join(packages), shell=True)
 
 except:
     print(colored("[Warning] Failed to upgrade package!", "yellow"))
 
-# Python Version scrapen
+# scrape Python version from website
 response = requests.get("https://www.python.org/downloads/")
 
 soup = BeautifulSoup(response.text, "html5lib")
 aTag = soup.findAll('a')[79]
 
-# Versionen formatieren
+# format the version
 txt = str(aTag).split("\n")[0].replace("</a>", "")
 txt = txt.split(" ")
 
@@ -28,7 +28,7 @@ localVersion = localVersion.replace(".", "")
 
 version = txt[len(txt) - 1].replace(".", "")
 
-# Versionen abgleichen
+# compare the versions
 if int(version) > int(localVersion):
     print(colored("\n[Warning] Python is outdated!", "yellow"))
 
